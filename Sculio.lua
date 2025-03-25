15b92c76d67bbc86d74b23d834975c2d161a8eda
Brandon Evans <contact@brandonmevans.com>
2025-03-25 13:54:04 -0500

    Gold Ore

--- Sculio.lua
+++ Sculio.lua
@@ -209,3 +209,37 @@ SMODS.Joker {
     end
   end
 }
+
+SMODS.Joker {
+  key = 'gold_ore',
+  loc_txt = {
+    name = 'Gold Ore',
+    text = {
+      'Scored Stone Cards',
+      'gain a Gold Seal'
+    }
+  },
+
+  unlocked = true,
+  discovered = true,
+  rarity = 2, -- Uncommon
+  atlas = 'Sculio',
+  pos = { x = 5, y = 0 },
+  cost = 7,
+  calculate = function(self, card, context)
+    if context.before and not context.blueprint then
+      for k, v in ipairs(context.scoring_hand) do
+        if v.config.center == G.P_CENTERS.m_stone and not v.debuff and not v.vampired then 
+          v:set_seal('Gold', nil, true)
+
+          G.E_MANAGER:add_event(Event({
+            func = function()
+              v:juice_up()
+              return true
+            end
+          }))
+        end
+      end
+    end
+  end
+}

14a375e60c71dec30ba4bcdb6127d6377c7f8c6a
Brandon Evans <contact@brandonmevans.com>
2025-03-25 14:15:53 -0500

    Pop Star

--- Sculio.lua
+++ Sculio.lua
@@ -215,8 +215,8 @@ SMODS.Joker {
   loc_txt = {
     name = 'Gold Ore',
     text = {
-      'Scored Stone Cards',
-      'gain a Gold Seal'
+      'Scored {C:attention}Stone Cards{}',
+      'gain a {C:attention}Gold Seal{}'
     }
   },
 
@@ -231,6 +231,48 @@ SMODS.Joker {
       for k, v in ipairs(context.scoring_hand) do
         if v.config.center == G.P_CENTERS.m_stone and not v.debuff and not v.vampired then 
           v:set_seal('Gold', nil, true)
+
+          G.E_MANAGER:add_event(Event({
+            func = function()
+              v:juice_up()
+              return true
+            end
+          }))
+        end
+      end
+    end
+  end
+}
+
+SMODS.Joker {
+  key = 'pop_star',
+  loc_txt = {
+    name = 'Pop Star',
+    text = {
+      '{C:green}#1# in #2#{} chance',
+      'for {C:attention}each scored card{}',
+      'to be {C:attention}enhanced{}'
+    }
+  },
+
+  config = { extra = { odds = 8 } },
+  unlocked = true,
+  discovered = true,
+  rarity = 2, -- Uncommon
+  atlas = 'Sculio',
+  pos = { x = 6, y = 0 },
+  cost = 8,
+  loc_vars = function(self, info_queue, card)
+    return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.odds, card.ability.extra.x_mult } }
+  end,
+  calculate = function(self, card, context)
+    if context.before and not context.blueprint then
+      for k, v in ipairs(context.scoring_hand) do
+        if v.config.center == G.P_CENTERS.c_base and pseudorandom('pop_star') < G.GAME.probabilities.normal / card.ability.extra.odds then
+          math.randomseed(pseudorandom('pop_star'))
+          enhancement_index = math.random(1, #G.P_CENTER_POOLS["Enhanced"])
+          enhancement = G.P_CENTER_POOLS["Enhanced"][enhancement_index]
+          v:set_ability(enhancement, nil, true)
 
           G.E_MANAGER:add_event(Event({
             func = function()

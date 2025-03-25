828365a8fe810a798f9240792f886074b36736e2
Brandon Evans <contact@brandonmevans.com>
2025-03-25 13:41:27 -0500

    Impossible Stairs.

--- Sculio.lua
+++ Sculio.lua
@@ -1,8 +1,8 @@
 SMODS.Atlas {
   -- Key for code to find it with
-  key = "Sculio",
+  key = 'Sculio',
   -- The name of the file, for the code to pull the atlas from
-  path = "Sculio.png", -- Original file sourced from https://github.com/Steamodded/examples/tree/master/Mods/ExampleJokersMod/assets
+  path = 'Sculio.png', -- Original file sourced from https://github.com/Steamodded/examples/tree/master/Mods/ExampleJokersMod/assets
   -- Width of each sprite in 1x size
   px = 71,
   -- Height of each sprite in 1x size
@@ -14,8 +14,8 @@ SMODS.Joker {
   loc_txt = {
     name = 'Schrodinger\'s Joker',
     text = {
-      "{C:green}#1# in #2#{} chance",
-      "for {X:mult,C:white}X#3#{} Mult"
+      '{C:green}#1# in #2#{} chance',
+      'for {X:mult,C:white}X#3#{} Mult'
     }
   },
 
@@ -27,10 +27,10 @@ SMODS.Joker {
   pos = { x = 0, y = 0 },
   cost = 4,
   loc_vars = function(self, info_queue, card)
-    return { vars = { (G.GAME.probabilities.normal or 1), self.config.extra.odds, self.config.extra.x_mult } }
+    return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.odds, card.ability.extra.x_mult } }
   end,
   calculate = function(self, card, context)
-    if context.joker_main and pseudorandom('schrodinger') < G.GAME.probabilities.normal / self.config.extra.odds then
+    if context.joker_main and pseudorandom('schrodinger') < G.GAME.probabilities.normal / card.ability.extra.odds then
       return {
         Xmult_mod = card.ability.extra.x_mult,
         message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.x_mult } }
@@ -40,14 +40,108 @@ SMODS.Joker {
 }
 
 SMODS.Joker {
+  key = 'impossible_stairs',
+  loc_txt = {
+    name = 'Impossible Stairs',
+    text = {
+      'This Joker gains between {C:mult}#3#{}',
+      'and {C:mult}#2#{} Mult per hand played.',
+      'Destroyed if Mult reaches {C:mult}#4#{} Mult',
+      '{C:inactive}(Currently {C:mult}#1#{}{C:inactive})'
+    }
+  },
+
+  config = { extra = { mult = 5, mult_add_min = -1, mult_add_max = 2, mult_min = 0 } },
+  unlocked = true,
+  discovered = true,
+  rarity = 1, -- Common
+  atlas = 'Sculio',
+  pos = { x = 1, y = 0 },
+  cost = 3,
+  loc_vars = function(self, info_queue, card)
+    mult_text = card.ability.extra.mult
+
+    if mult_text >= 0 then
+      mult_text = '+' .. mult_text
+    end
+
+    mult_add_min_text = card.ability.extra.mult_add_min
+
+    if mult_add_min_text >= 0 then
+      mult_add_min_text = '+' .. mult_add_min_text
+    end
+
+    mult_add_max_text = card.ability.extra.mult_add_max
+
+    if mult_add_max_text >= 0 then
+      mult_add_max_text = '+' .. mult_add_max_text
+    end
+
+    mult_min_text = card.ability.extra.mult_min
+
+    if mult_min_text >= 0 then
+      mult_min_text = '+' .. mult_min_text
+    end
+
+    return { vars = { mult_text, mult_add_min_text, mult_add_max_text, mult_min_text } }
+  end,
+  calculate = function(self, card, context)
+    if context.joker_main then
+      return {
+        mult_mod = card.ability.extra.mult,
+        message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+      }
+    end
+
+    if context.after and not context.blueprint then
+      math.randomseed(pseudorandom('impossible_stairs'))
+      add = math.random(card.ability.extra.mult_add_min, card.ability.extra.mult_add_max)
+      card.ability.extra.mult = card.ability.extra.mult + add
+
+      if card.ability.extra.mult <= card.ability.extra.mult_min then
+        -- Delete Joker. Copied from Ice Cream.
+        G.E_MANAGER:add_event(Event({
+          func = function()
+            play_sound('tarot1')
+            card.T.r = -0.2
+            card:juice_up(0.3, 0.4)
+            card.states.drag.is = true
+            card.children.center.pinch.x = true
+
+            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
+            func = function()
+              G.jokers:remove_card(card)
+              card:remove()
+              card = nil
+              return true
+            end
+          }))
+
+          return true
+          end
+        }))
+      end
+
+      mult_text = card.ability.extra.mult
+
+      if mult_text >= 0 then
+        mult_text = '+' .. mult_text
+      end
+
+      return { message = mult_text }
+    end
+  end
+}
+
+SMODS.Joker {
   key = 'house_party',
   loc_txt = {
     name = 'House Party',
     text = {
-      "This Joker gains {X:mult,C:white}X#2#{} Mult",
-      "per {C:attention}consecutive{} hand played",
-      "containing a {C:attention}Full House{}",
-      "{C:inactive}(Currently {X:mult,C:white}X#1#{}{C:inactive})",
+      'This Joker gains {X:mult,C:white}X#2#{} Mult',
+      'per {C:attention}consecutive{} hand played',
+      'containing a {C:attention}Full House{}',
+      '{C:inactive}(Currently {X:mult,C:white}X#1#{}{C:inactive})',
     }
   },
 
@@ -85,9 +179,9 @@ SMODS.Joker {
   loc_txt = {
     name = 'Wall Street Joker',
     text = {
-      "The {C:mult}+Mult{} for this Joker",
-      "{C:attention}doubles{} after every boss blind",
-      "{C:inactive}(Currently {C:mult}+#1#{}{C:inactive})"
+      'The {C:mult}+Mult{} for this Joker',
+      '{C:attention}doubles{} after every boss blind',
+      '{C:inactive}(Currently {C:mult}+#1#{}{C:inactive})'
     }
   },
 

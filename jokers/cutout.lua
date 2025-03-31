797c2dac8ce2db557d040452d900de63fc478302
Brandon Evans <contact@brandonmevans.com>
2025-03-31 05:46:50 -0500

    Cutout

---
+++ jokers/cutout.lua
@@ -0,0 +1,39 @@
+SMODS.Joker {
+  key = 'cutout',
+  loc_txt = {
+    name = 'Joker Cutout',
+    text = {
+      "{X:red,C:white} X#1# {} Mult for each",
+      "{C:attention}other{} occupied {C:attention}Joker{} slot",
+      "{C:inactive}(Currently {X:red,C:white} X#2# {C:inactive})",
+    }
+  },
+
+  config = { extra = { x_mult = 0.5 } },
+  unlocked = true,
+  discovered = true,
+  rarity = 3, -- Rare
+  atlas = 'Sculio',
+  pos = { x = 2, y = 1 },
+  cost = 8,
+  get_x_mult_mod = function(self)
+    if G.jokers and #G.jokers.cards > 1 then
+      return 1 + (self.config.extra.x_mult * (#G.jokers.cards - 1))
+    else
+      return 1
+    end
+  end,
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.x_mult, self:get_x_mult_mod(self) } }
+  end,
+  calculate = function(self, card, context)
+    x_mult_mod = self:get_x_mult_mod(self)
+
+    if context.joker_main and x_mult_mod > 1 then
+      return {
+        Xmult_mod = x_mult_mod,
+        message = localize { type = 'variable', key = 'a_xmult', vars = { x_mult_mod } }
+      }
+    end
+  end
+}

7b92020be782db98474d355f34e67618c8dd5ed0
Brandon Evans <contact@brandonmevans.com>
2025-04-09 10:10:29 -0500

    Receipt

---
+++ jokers/20_receipt.lua
@@ -0,0 +1,30 @@
+SMODS.Joker {
+  key = 'receipt',
+  loc_txt = {
+    name = 'Receipt',
+    text = {
+      "Sell this card to",
+      "create a free",
+      "{C:attention}Voucher Tag{}"
+    }
+  },
+
+  unlocked = true,
+  discovered = true,
+  rarity = 2, -- Uncommon
+  atlas = 'Sculio',
+  pos = { x = 1, y = 2 },
+  cost = 8,
+  calculate = function(self, card, context)
+    if context.selling_self then
+      G.E_MANAGER:add_event(Event({
+        func = (function()
+          add_tag(Tag('tag_voucher'))
+          play_sound('generic1', 0.9 + math.random()*0.1, 0.8)
+          play_sound('holo1', 1.2 + math.random()*0.1, 0.4)
+          return true
+        end)
+    }))
+    end
+  end
+}

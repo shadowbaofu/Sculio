0aa7324e94bfe8ed9c1ee1593aec31f8a7ccc178
Brandon Evans <contact@brandonmevans.com>
2025-04-09 21:03:22 -0500

    Jokerium

---
+++ jokers/23_jokerium.lua
@@ -0,0 +1,65 @@
+SMODS.Joker {
+  key = 'jokerium',
+  loc_txt = {
+    name = 'Jokerium',
+    text = {
+      'Adds {C:attention}#1#{} {C:dark_edition}Negative{}',
+      ' {C:spectral}Black Hole{} cards each time',
+      '{C:attention}#2# {C:attention}Boss Blinds{} are defeated',
+      '{C:inactive}#3# remaining{}',
+    }
+  },
+
+  config = { extra = { black_holes_per_cycle = 2, boss_blinds_per_cycle = 3, boss_blinds_since_last_cycle = 0 } },
+  unlocked = true,
+  discovered = true,
+  rarity = 2, -- Uncommon
+  atlas = 'Sculio',
+  pos = { x = 4, y = 2 },
+  cost = 7,
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.black_holes_per_cycle, card.ability.extra.boss_blinds_per_cycle, card.ability.extra.boss_blinds_per_cycle - card.ability.extra.boss_blinds_since_last_cycle } }
+  end,
+  calculate = function(self, card, context)
+    if context.joker_main then
+      return {
+        mult_mod = card.ability.extra.mult,
+        message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+      }
+    end
+
+    if context.end_of_round and not context.repetition and context.game_over == false and not context.blueprint and G.GAME.blind.boss then
+      card.ability.extra.boss_blinds_since_last_cycle = card.ability.extra.boss_blinds_since_last_cycle + 1
+
+      if card.ability.extra.boss_blinds_since_last_cycle >= card.ability.extra.boss_blinds_per_cycle then
+        card.ability.extra.boss_blinds_since_last_cycle = 0
+
+        G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + card.ability.extra.black_holes_per_cycle
+
+        for i = 1, card.ability.extra.black_holes_per_cycle, 1 do
+          G.E_MANAGER:add_event(Event({
+            delay = 0.0,
+            func = function()
+              local card = create_card('Spectral', G.consumeables, nil, nil, nil, nil, 'c_black_hole')
+              card:set_edition({negative = true}, true)
+              card:add_to_deck()
+              G.consumeables:emplace(card)
+              G.GAME.consumeable_buffer = 0
+              return true
+            end
+          }))
+        end
+
+        return {
+            message = '+' .. card.ability.extra.black_holes_per_cycle .. ' Black Hole' .. ((card.ability.extra.black_holes_per_cycle == 1 and '') or 's'),
+            colour = G.C.SECONDARY_SET.Spectral
+        }
+      else
+        return {
+          message = card.ability.extra.boss_blinds_since_last_cycle .. '/' .. card.ability.extra.boss_blinds_per_cycle,
+          colour = G.C.FILTER
+        }
+      end
+    end
+  end
+}

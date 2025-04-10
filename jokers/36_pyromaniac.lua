2b6c965ede175152e9f4e058435dbcf16957ec22
Brandon Evans <contact@brandonmevans.com>
2025-04-09 22:30:46 -0500

    Pyromaniac

---
+++ jokers/36_pyromaniac.lua
@@ -0,0 +1,70 @@
+SMODS.Joker {
+  key = 'pyromaniac',
+  loc_txt = {
+    name = 'Pyromaniac',
+    text = {
+      'If {C:attention}first hand{} of round is your',
+      '{C:attention}most played hand{}, {C:attention}destroy{}',
+      '{C:attention}scored cards{}, and {C:attention}level{}',
+      '{C:attention}up hand #1# times{}'
+    }
+  },
+
+  config = { extra = { levels_to_increase = 2 } },
+  unlocked = true,
+  discovered = true,
+  rarity = 3, -- Rare
+  atlas = 'Sculio',
+  pos = { x = 7, y = 3 },
+  cost = 10,
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.levels_to_increase } }
+  end,
+  calculate = function(self, card, context)
+    if context.first_hand_drawn and not context.blueprint then
+      local eval = function() return G.GAME.current_round.hands_played == 0 end
+      juice_card_until(card, eval, true)
+    end
+
+    if context.before then
+      card.ability.cards_to_burn = {}
+
+      if G.GAME.current_round.hands_played == 0 then
+        -- Based off of Obelisk.
+        most_played = true
+        most_played_count = (G.GAME.hands[context.scoring_name].played or 0)
+
+        for k, v in pairs(G.GAME.hands) do
+          if k ~= context.scoring_name and v.played >= most_played_count and v.visible then
+            most_played = false
+          end
+        end
+
+        if most_played then
+          local text, disp_text = context.scoring_name
+
+          for i = 1, card.ability.extra.levels_to_increase, 1 do
+            card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_upgrade_ex')})
+            update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {handname=localize(text, 'poker_hands'),chips = G.GAME.hands[text].chips, mult = G.GAME.hands[text].mult, level=G.GAME.hands[text].level})
+            level_up_hand(context.blueprint_card or card, text, nil, 1)
+            update_hand_text({sound = 'button', volume = 0.7, pitch = 1.1, delay = 0}, {mult = 0, chips = 0, handname = '', level = ''})
+          end
+
+          card.ability.cards_to_burn = context.scoring_hand
+        end
+      end
+    end
+
+    if context.after then
+      G.E_MANAGER:add_event(Event({
+        func = function()
+          for _, card_to_burn in pairs(card.ability.cards_to_burn) do
+            card_to_burn:start_dissolve({G.C.GOLD})
+          end
+
+          return true
+        end
+      }))
+    end
+  end
+}

9b17797f3df932b4917038f3c69c6451fad4fe4d
Brandon Evans <contact@brandonmevans.com>
2025-07-28 09:02:07 -0500

    Fixed Hammer and Chisel, changed functionality.

--- jokers/29_hammer_and_chisel.lua
+++ jokers/29_hammer_and_chisel.lua
@@ -3,14 +3,14 @@ SMODS.Joker {
   loc_txt = {
     name = 'Hammer and Chisel',
     text = {
-      'Scored {C:attention}Stone Cards{} give',
-      '{C:chips}+#3#{} chips and {C:mult}+#4#{} Mult with',
-      'a {C:green}#1# in #2#{} chance of {C:attention}being{}',
-      '{C:attention}destroyed{}',
+      'Scored {C:attention}Stone Cards{}',
+      'permanently gain {C:chips}+#3#{} chips',
+      'with a {C:green}#1# in #2#{} chance',
+      'of {C:attention}being destroyed{}',
     }
   },
 
-  config = { extra = { chip_bonus = 10, mult_bonus = 2, shatter_odds = 10 } },
+  config = { extra = { chip_perma_bonus = 15, shatter_odds = 10 } },
   unlocked = true,
   discovered = false,
   rarity = 1, -- Common
@@ -20,21 +20,25 @@ SMODS.Joker {
   blueprint_compat = true,
   loc_vars = function(self, info_queue, card)
     info_queue[#info_queue+1] = G.P_CENTERS.m_stone
-    return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.shatter_odds, card.ability.extra.chip_bonus, card.ability.extra.mult_bonus } }
+    return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.shatter_odds, card.ability.extra.chip_perma_bonus } }
   end,
   calculate = function(self, card, context)
     if context.before then
       card.ability.cards_to_shatter = {}
     end
 
-    if context.individual and context.cardarea == G.play and context.other_card.debuff and context.other_card.config.center == G.P_CENTERS.m_stone then
+    if context.individual and context.cardarea == G.play and not context.other_card.debuff and context.other_card.config.center == G.P_CENTERS.m_stone then
       if pseudorandom('hammer_and_chisel') < G.GAME.probabilities.normal / card.ability.extra.shatter_odds then
         table.insert(card.ability.cards_to_shatter, context.other_card)
       end
 
+      -- Based off of Hiker.
+      context.other_card.ability.perma_bonus = context.other_card.ability.perma_bonus or 0
+      context.other_card.ability.perma_bonus = context.other_card.ability.perma_bonus + card.ability.extra.chip_perma_bonus
+
       return {
-        chips = card.ability.extra.chip_bonus,
-        mult = card.ability.extra.mult_bonus,
+        extra = {message = localize('k_upgrade_ex'), colour = G.C.CHIPS},
+        colour = G.C.CHIPS,
         card = card
       }
     end

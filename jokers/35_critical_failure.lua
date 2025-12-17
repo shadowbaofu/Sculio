b9a3339998463520b06699991f5e4fc1a088df6e
shadowbaofu <134404402+shadowbaofu@users.noreply.github.com>
2025-12-17 06:34:24 -0600

    Remove enhancement_gate from critical_failure card
    Removed enhancement_gate property from the card.

--- jokers/35_critical_failure.lua
+++ jokers/35_critical_failure.lua
@@ -8,7 +8,6 @@ SMODS.Joker {
   atlas = 'Sculio',
   pos = { x = 6, y = 3 },
   cost = 8,
-  enhancement_gate = 'm_lucky',
   blueprint_compat = true,
   loc_vars = function(self, info_queue, card)
     info_queue[#info_queue+1] = G.P_CENTERS.m_lucky
